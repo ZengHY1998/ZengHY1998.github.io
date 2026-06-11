1e51618e6f1cfa0c741e59340819fa7a8496b631
ZengHY1998 <zhy.cqupt@gmail.com>
2026-06-11 23:37:05 +0900

    Bug fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,8 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://zenghy1998.github.io',
+	base: '/',
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
